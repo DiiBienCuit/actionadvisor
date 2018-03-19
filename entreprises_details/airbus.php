c728081bd5c8ae0ce66621f5e29375feb134e91a
DI CUI <cuidi.fr@gmail.com>
2018-03-19 12:51:00 +0100

    Add files via upload

--- entreprises_details/airbus.php
+++ entreprises_details/airbus.php
@@ -8,25 +8,23 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+  <script type="text/javascript" src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="../airbus.js"></script>
     <title>AIRBUS</title>
-    <script>
-    function updateData(data){
-        console.log(data.dataset_data.data[1][0]);
-        console.log(data.dataset_data.data[1][4]);
-    }
-    $.getJSON("https://www.quandl.com/api/v3/datasets/EURONEXT/BN/data.json?api_key=Zvxsyh15Zw6CniyvnpoL&start_date=2018-02-01&end_date=2018-03-13&order=asc", updateData);
-    
-    </script>
     <!-- Bootstrap core CSS -->
     <link href="https://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="css/index.css" rel="stylesheet">
+    <script>  
+    var interval = 60;
+    </script>
   </head>
 
 <body>
@@ -36,7 +34,7 @@
       <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
-          <a class="nav-link" href="../index.php">Sign out</a>
+          <a class="nav-link" href="../index.php">Se deconnecter</a>
         </li>
       </ul>
     </nav>
@@ -99,34 +97,22 @@
             </ul>
           </div>
         </nav>
-
+        
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
             <h1 class="h2">Dashboard Airbus (AIR)</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
               <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary">Bollinger</button>
-                <button class="btn btn-sm btn-outline-secondary">Moyenne mobile</button>
-                <button class="btn btn-sm btn-outline-secondary">RSI</button>
-                <button class="btn btn-sm btn-outline-secondary">Momentums</button>
+                <button class="btn btn-sm btn-outline-secondary">Cours</button>
+                <button class="btn btn-sm btn-outline-secondary">Bollingers</button>
               </div>
-              <div class="dropdown">
-              <button class="btn btn-sm btn-outline-secondary dropdown-toggle" data-toggle="dropdown">
-                <span data-feather="calendar"></span>
-                Interval
-              </button>
-              <ul class="dropdown-menu">
-                <li><a href="#">7 jours</a></li>
-                <li><a href="#">1 mois</a></li>
-                <li><a href="#">3 mois</a></li>
-                <li><a href="#">6 mois</a></li>
-            </ul>  
-            </div>
+
             </div>
           </div>
-            
-          <canvas class="my-4" id="myChart" width="900" height="380"></canvas>
-
+        
+            <h4>Conseil:</h4>
+            <h4 class="conseil" id="conseil-airbus">En train d'analyser..</h4>
+         <div id="chartContainer" style="height: 300px; width: 100%;"></div>   
           <h2>Nouvelles de l'entreprise</h2>
           <div class="table-responsive">
             <table class="table table-striped table-sm">
@@ -140,25 +126,32 @@
               </thead>
               <tbody>
                 <tr>
-                  <td>1</td>
-                  <td>taciti</td>
-                  <td>sociosqu</td>
+                  <td id="time1"></td>
+                  <td id="title1"></td>
+                  <td id="des1"></td>
+                  <td id="note1">
+                      <form action="../newsNote.php">
+ <input type="text" name="airbusNote1" value=""><br>
+<input type="submit" value="Valider">
+</form></td>
                 </tr>
                 <tr>
-                  <td>1,013</td>
-                  <td>torquent</td>
-                  <td>per</td>
+                  <td id="time2"></td>
+                  <td id="title2"></td>
+                  <td id="des2"></td>
+                  <td id="note2">                      <form action="../newsNote.php">
+ <input type="text" name="airbusNote2" value=""><br>
+<input type="submit" value="Valider">
+</form></td>
                 </tr>
                 <tr>
-                  <td>1,014</td>
-                  <td>per</td>
-                  <td>Curabitur</td>
-                </tr>
-                <tr>
-                  <td>1,015</td>
-                  <td>sodales</td>
-                  <td>ligula</td>
-                
+                  <td id="time3"></td>
+                  <td id="title3"></td>
+                  <td id="des3"></td>
+                  <td id="note3">                      <form action="../newsNote.php">
+ <input type="text" name="airbusNote3" value=""><br>
+<input type="submit" value="Valider">
+</form></td>
                 </tr>
               </tbody>
             </table>
@@ -172,7 +165,6 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../../../assets/js/vendor/popper.min.js"></script>
     <script src="../../../../dist/js/bootstrap.min.js"></script>
 
     <!-- Icons -->
@@ -180,37 +172,37 @@
     <script>
       feather.replace()
     </script>
-
+   
+  
     <!-- Graphs -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+    <script type="text/javascript" scr="../airbus.js"></script>
     <script>
-      var ctx = document.getElementById("myChart");
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-          datasets: [{
-            data: [15339, 21345, 18483, 24003, 23489, 24092, 15555],
-            lineTension: 0,
-            backgroundColor: 'transparent',
-            borderColor: '#007bff',
-            borderWidth: 4,
-            pointBackgroundColor: '#007bff'
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: false
-              }
-            }]
-          },
-          legend: {
-            display: false,
-          }
-        }
-      });
+    var dataPoints =[];
+var chart = new CanvasJS.Chart("chartContainer", {
+animationEnabled: true,
+exportEnabled: true,
+responsive:true,
+zoomEnabled: true,
+title: {
+    text: "Cours - Airbus"
+},
+toolTip: {
+		shared: true
+	},
+axisY: {
+    title: "Price",
+    includeZero: false
+},
+data: [{
+    type: "line",
+    name:"cours",
+    color: "#DD7E86",
+    yValueFormatString: "€##0.00",
+    dataPoints: dataPoints
+}]
+});
+
+//$.getJSON("https://www.quandl.com/api/v3/datasets/EURONEXT/BN.json?api_key=Zvxsyh15Zw6CniyvnpoL&start_date=2017-12-20&order=desc", addData);
     </script>
   </body>
 </html>
